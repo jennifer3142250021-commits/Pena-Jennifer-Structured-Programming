976303fcdc88b403faf7420a0bab63fa961e0210
jennifer3142250021-commits <jennifer_3142250021@utd.edu.mx>
2026-05-11 13:09:43 -0600

    Add files via upload

---
+++ UNIT 1/lab1.py
@@ -0,0 +1,23 @@
+#code A
+grades= [80,90,70]
+average=sum(grades) / len(grades)
+print(average)
+
+#code B
+def calculate_average(data):
+    return sum(data) / len(data)
+grades - [80,90,70]
+print(calculate_average (grades))
+
+#Code C
+numbers=[1,2,3,4,5]
+squares= list (map(lambda x: x**2, numbers))
+print(squares)
+
+
+
+
+#Modify code
+grades=[60,70,10,90,80]
+average=sum(grades)/len(grades)
+print(average)
