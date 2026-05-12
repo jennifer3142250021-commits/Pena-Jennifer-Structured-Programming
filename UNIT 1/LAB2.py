bf209090aa8d7e0c21f8c18d0d14019d586c854f
jennifer3142250021-commits <jennifer_3142250021@utd.edu.mx>
2026-05-12 17:34:32 -0600

    Add files via upload

---
+++ UNIT 1/LAB2.py
@@ -0,0 +1,28 @@
+#CODE 1
+def multiply(a, b):
+    return a * b
+print(multiply(3,4))
+
+#CODE 2
+counter = 0
+for i in range(4):
+    counter = counter + 1
+    print(counter)
+
+    
+#CODE 3
+age = int(input("Enter your age: "))
+if age >= 18: #i fix the code by adding :
+    print("Adult")
+
+
+#CODE 4
+a = 3
+b = 4
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
+
+   
